ba18b39920931415f9a18ff40e0d326849c3b928
Yaroslav Prikhodin <4yorick@gmail.com>
2024-11-18 23:47:50 +0300

    Fixed running when no sets.json

--- RandomPickerUI/MainWindowViewModel.cs
+++ RandomPickerUI/MainWindowViewModel.cs
@@ -105,16 +105,52 @@ namespace RandomPickerUI
         public MainWindowViewModel()
         {
             string fileName = "Sets.json";
-            string jsonString = File.ReadAllText(fileName);
-            List<Set> SetsFromJson  = JsonSerializer.Deserialize<List<Set>>(jsonString)!;
-            sets = new ObservableCollection<Set>(SetsFromJson);
-            currentSet = Sets.First();
-            currentItem = Sets.First().Items.First();
-            currentGroup = currentSet.Groups.First();
-            currentGroupItem = currentGroup.Items.First();
-            resultString = new ObservableCollection<string>();
-            TimesFromSet = 1;
-            TimesFromGroup = 1;
+            if (File.Exists(fileName))
+            {
+                string jsonString = File.ReadAllText(fileName);
+                List<Set> SetsFromJson = JsonSerializer.Deserialize<List<Set>>(jsonString)!;
+                sets = new ObservableCollection<Set>(SetsFromJson);
+                currentSet = Sets.First();
+                currentItem = Sets.First().Items.First();
+                currentGroup = currentSet.Groups.First();
+                currentGroupItem = currentGroup.Items.First();
+                resultString = new ObservableCollection<string>();
+                TimesFromSet = 1;
+                TimesFromGroup = 1;
+            }
+            else
+            {
+                sets = new ObservableCollection<Set>();
+                var testSet = new Set("Test");
+                testSet.Items.Add(new Item("One"));
+                testSet.Items.Add(new Item("Two"));
+                testSet.Items.Add(new Item("Three"));
+                testSet.Items.Add(new Item("Four"));
+                testSet.Items.Add(new Item("Five"));
+                var group1 = new Group("group 1");
+                var group2 = new Group("group 2");
+                var testArray = testSet.Items.ToArray();
+                group1.Items.Add(testArray[0]);
+                group1.Items.Add(testArray[1]);
+                group1.Items.Add(testArray[2]);
+                testSet.Groups.Add(group1);
+                group2.Items.Add(testArray[2]);
+                group2.Items.Add(testArray[4]);
+                testSet.Groups.Add(group2);
+                Sets.Add(testSet);
+                var testSet2 = new Set("Test2");
+                testSet2.Items.Add(new Item("One 2"));
+                testSet2.Items.Add(new Item("Two 2"));
+                testSet2.Items.Add(new Item("Three 2"));
+                Sets.Add(testSet2);
+                currentSet = Sets.First();
+                currentItem = Sets.First().Items.First();
+                currentGroup = currentSet.Groups.First();
+                currentGroupItem = currentGroup.Items.First();
+                resultString = new ObservableCollection<string>();
+                TimesFromSet = 1;
+                TimesFromGroup = 1;
+            }
         }
         public void PickFromSet()
         {
